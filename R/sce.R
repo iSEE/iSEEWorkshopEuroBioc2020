052991139a4266e7304e2a2f6cb36f30864e0839
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-11-13 20:59:15 +0000

    add function to simulate RDS file for examples

--- R/sce.R
+++ R/sce.R
@@ -2,10 +2,14 @@
 #'
 #' @param name Data set identifier. Only `pbmc3k` is available at the moment.
 #'
-#' @return A `SingleCellExperiment` object.
+#' @return
+#' `simulate_demo_sce()` simulates RDS files usually provided in the [Docker container](https://hub.docker.com/r/iseedevelopers/iseeworkshopeurobioc2020).
+#' 
+#' `demo_sce()` returns a `SingleCellExperiment` object.
 #' @export
 #'
 #' @examples
+#' simulate_demo_sce("pbmc3k")
 #' sce <- demo_sce("pbmc3k")
 demo_sce <- function(name = c("pbmc3k")) {
     name <- match.arg(name)
@@ -16,12 +20,11 @@ demo_sce <- function(name = c("pbmc3k")) {
     sce
 }
 
-#' Simulate Demonstration Data Sets
-#'
-#' @param name Data set identifier. Only `pbmc3k` is available at the moment.
-#'
-#' @return `TRUE`
-.simulate_demo_sce <- function(name = c("pbmc3k")) {
+#' @rdname demo_sce
+#' @export
+#' 
+#' @importFrom SingleCellExperiment SingleCellExperiment
+simulate_demo_sce <- function(name = c("pbmc3k")) {
     name <- match.arg(name)
     user_home <- path.expand("~")
     base_file <- sprintf("%s.rds", name)
