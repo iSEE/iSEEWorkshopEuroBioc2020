77838dc6349ddb229e614e5fc60640708639fac6
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-11-13 19:29:03 +0000

    internal function to generate demo file

--- R/sce.R
+++ R/sce.R
@@ -15,3 +15,22 @@ demo_sce <- function(name = c("pbmc3k")) {
     sce <- readRDS(file_sce)
     sce
 }
+
+#' Simulate Demonstration Data Sets
+#'
+#' @param name Data set identifier. Only `pbmc3k` is available at the moment.
+#'
+#' @return `TRUE`
+.simulate_demo_sce <- function(name = c("pbmc3k")) {
+    name <- match.arg(name)
+    user_home <- path.expand("~")
+    base_file <- sprintf("%s.rds", name)
+    dir_sce <- file.path(user_home, "iseeworkshopeurobioc2020", "vignettes")
+    file_sce <- file.path(dir_sce, base_file)
+    # In the Docker container, the file should already exist
+    if (!file.exists(file_sce)) {
+        dir.create(dir_sce, recursive = TRUE)
+        saveRDS(SingleCellExperiment(), file_sce)
+    }
+    TRUE
+}
--- tests/testthat/test-sce.R
+++ tests/testthat/test-sce.R
@@ -1,13 +1,9 @@
+
+
 test_that("multiplication works", {
-    # Simulate the situation in the Docker container
-    user_home <- path.expand("~")
-    dir_sce <- file.path(user_home, "iseeworkshopeurobioc2020", "vignettes")
-    file_sce <- file.path(dir_sce, "pbmc3k.rds")
-    # In the Docker container, the file should already exist
-    if (!file.exists(file_sce)) {
-        dir.create(dir_sce, recursive = TRUE)
-        saveRDS(SingleCellExperiment(), file_sce)
-    }
+    expect_true(
+        iSEEWorkshopEuroBioc2020:::.simulate_demo_sce("pbmc3k")
+    )
     
     expect_s4_class(
         demo_sce("pbmc3k"),
