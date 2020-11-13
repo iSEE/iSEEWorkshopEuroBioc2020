8cffa71a5b86bc931d7045774a1090ea3598605e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-11-13 21:36:22 +0000

    fix function name in tests

--- tests/testthat/test-sce.R
+++ tests/testthat/test-sce.R
@@ -2,7 +2,7 @@
 
 test_that("multiplication works", {
     expect_true(
-        iSEEWorkshopEuroBioc2020:::.simulate_demo_sce("pbmc3k")
+        simulate_demo_sce("pbmc3k")
     )
     
     expect_s4_class(
