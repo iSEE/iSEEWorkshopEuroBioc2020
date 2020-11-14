4f2e82b161fc1dc85861433c76160ba0efb337cc
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-11-14 10:22:12 +0000

    move simulation functions to test files

--- R/sce.R
+++ R/sce.R
@@ -2,15 +2,14 @@
 #'
 #' @param name Data set identifier. Only `pbmc3k` is available at the moment.
 #'
-#' @return
-#' `simulate_demo_sce()` simulates RDS files usually provided in the [Docker container](https://hub.docker.com/r/iseedevelopers/iseeworkshopeurobioc2020).
-#' 
-#' `demo_sce()` returns a `SingleCellExperiment` object.
+#' @return A `SingleCellExperiment` object.
 #' @export
 #'
 #' @examples
-#' simulate_demo_sce("pbmc3k")
-#' sce <- demo_sce("pbmc3k")
+#' # This example only works on the Docker container for the workshop
+#' if (interactive()) {
+#'     sce <- demo_sce("pbmc3k")
+#' }
 demo_sce <- function(name = c("pbmc3k")) {
     name <- match.arg(name)
     user_home <- path.expand("~")
@@ -18,21 +17,4 @@ demo_sce <- function(name = c("pbmc3k")) {
     file_sce <- file.path(user_home, "iseeworkshopeurobioc2020", "vignettes", base_file)
     sce <- readRDS(file_sce)
     sce
-}
-
-#' @rdname demo_sce
-#' @export
-#' 
-#' @importFrom SingleCellExperiment SingleCellExperiment
-simulate_demo_sce <- function(name) {
-    user_home <- path.expand("~")
-    base_file <- sprintf("%s.rds", name)
-    dir_sce <- file.path(user_home, "iseeworkshopeurobioc2020", "vignettes")
-    file_sce <- file.path(dir_sce, base_file)
-    dir.create(dir_sce, recursive = TRUE)
-    if (file.exists(file_sce)) {
-        stop("File already exists: ", file_sce)
-    }
-    saveRDS(SingleCellExperiment(), file_sce)
-    TRUE
 }
--- tests/testthat/setup-library.R
+++
@@ -1,1 +0,0 @@
-library(SingleCellExperiment)
